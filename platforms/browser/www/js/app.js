f182a583804c3ecd43b119e871f166d3a939e16c
Jakob Unnebäck <jakob@folkdatorn.se>
2017-04-15 11:11:54 +0200

    Feature: Complete tasks
    Complete button to finish assigned tasks.

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -7,14 +7,24 @@ function add (type) {
   var input = document.getElementById('input').value
 
   if (input === '') {
-    document.getElementById('input').style.backgroundcolor = "red"
+    document.getElementById('input').style.background = 'rgba(255, 170, 150, 1)'
   } else {
-    if (type == 'do') {
+    document.getElementById('input').style.background = 'rgba(255, 255, 250, 1)'
+    if (type === 'do') {
       dos.push(input)
-    } else if (type == 'dont') {
+    } else if (type === 'dont') {
       donts.push(input)
     }
     render()
+  }
+}
+
+function done (assignment) {
+  for (var i = 0; i < dos.lenght; i++) {
+    if (dos[i] === assignment) {
+      dos.splice(i, 1)
+      render()
+    }
   }
 }
 
@@ -22,10 +32,12 @@ function render () {
   var htmlDos = ''
   var htmlDonts = ''
   dos.forEach(function (item) {
-    htmlDos = htmlDos + '<li style="color: #89d877;">' + item + '</li>'
+    // htmlDos = htmlDos + '<li style="color: #89d877;">' + item + '</li>'
+    htmlDos = htmlDos + '<li style="color: #89d877;">' + item + '<button type="button" name="button" onclick="done(' + item + ')">&#x2713;</button></li>'
   })
   donts.forEach(function (item) {
-    htmlDonts = htmlDonts + '<li style="color: #f47442;">' + item + '</li>'
+    // htmlDonts = htmlDonts + '<li style="color: #f47442;">' + item + '</li>'
+    htmlDonts = htmlDonts + '<li style="color: #f47442;">' + item + '<button type="button" name="button" onclick="done("' + item + '")">&#x2713;</button></li>'
   })
   doElement.innerHTML = htmlDos
   dontElement.innerHTML = htmlDonts
